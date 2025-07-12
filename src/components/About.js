e71884a9fc426d1470a8b22091ec148f4150f19f
Angellucy460 <angelliona38@gmail.com>
2025-07-12 23:59:59 +0300

    changed files

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about"> 
+    <h2>About Me</h2>
+    <p>Hi, I am Angel, a full stack website developer with a passion for building responsive, user-friendly web applications from the ground up. I specialize in both front-end technologies like React and HTML/CSS, and back-end frameworks like Node.js and Express. I love turning ideas into interactive experiences that drive real impact.</p>
+    <img src={image} alt="I made this" />
+  </div>
+  );
 }
 
 export default About;
